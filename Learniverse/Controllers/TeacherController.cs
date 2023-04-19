a20e44bc7c9c6a9ba1b08f221d424284defbd35f
Md.Raihanul Islam Rafi <19-41790-3@student.aiub.edu>
2023-04-19 15:59:26 +0600

    kk

--- Learniverse/Controllers/TeacherController.cs
+++ Learniverse/Controllers/TeacherController.cs
@@ -10,5 +10,22 @@ namespace Learniverse.Controllers
     public class TeacherController : ApiController
     {
         /// sfdfsdf
+        /// dfsdf
+        /// dsfdsfsdfsd
+        /// HttpGet]
+        [Route("api/products")]
+        public HttpResponseMessage Get()
+        {
+            try
+            {
+                //var data = ProductService.Get();
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+
+        }
     }
 }
