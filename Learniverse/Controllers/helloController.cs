7de51722842bbb292b14132ea01e1ad8f6ef29a3
Md Abu Sufian <19-41749-3@student.aiub.edu>
2023-04-20 11:21:25 +0600

    delete stuCont

--- Learniverse/Controllers/helloController.cs
+++ Learniverse/Controllers/helloController.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
+﻿using System.Web.Http;
 
 namespace Learniverse.Controllers
 {
     public class helloController : ApiController
     {
+        public string name { get; set; }
     }
 }
