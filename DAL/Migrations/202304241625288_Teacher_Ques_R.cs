fd96ff79d897a5ad905ff5e6b7a01d920cd43790
Md.Raihanul Islam Rafi <19-41790-3@student.aiub.edu>
2023-04-24 22:53:52 +0600

    Relation on process

---
+++ DAL/Migrations/202304241625288_Teacher_Ques_R.cs
@@ -0,0 +1,22 @@
+﻿namespace DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Teacher_Ques_R : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Teachers", "QId", c => c.Int(nullable: false));
+            CreateIndex("dbo.Teachers", "QId");
+            AddForeignKey("dbo.Teachers", "QId", "dbo.Ques", "Id", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Teachers", "QId", "dbo.Ques");
+            DropIndex("dbo.Teachers", new[] { "QId" });
+            DropColumn("dbo.Teachers", "QId");
+        }
+    }
+}
--- DAL/Models/Course.cs
+++ DAL/Models/Course.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,16 @@ namespace DAL.Models
 {
     public class Course
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Category { get; set; }
         public float Price { get; set; }
+        public virtual ICollection<Teacher> Teachers { get; set; }
+        public Course()
+        {
+            Teachers = new List<Teacher>();
+        }
+
     }
 }
